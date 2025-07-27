9f4577a9a3a95f8a8798f089e6a9f98df6678554
Programmer <programmerbaharev@yandex.ru>
2025-07-27 16:22:20 +0400

    Draw first half-square

--- Ping Pong/Ping Pong.cpp
+++ Ping Pong/Ping Pong.cpp
@@ -6,58 +6,67 @@
 #include "raylib.h"
 
 // Functions signature
-int randomFunction();
+int randomFunction(int minValue, int maxValue);
 
 void main()
 {
-	std::cout << "Hello World!" << std::endl;
-
     // Initialization
+    // Ball settings
     const int ballRadius = 20;
+    const float ballSpeed = 2.0f;
+    // Screen settings
+    const int screenFPS = 60;
     const int screenWidth = 800;
     const int screenHeight = 450;
+    // HalfSquare settings
+    const int halfSquareWidth = 20;
+    const int halfSquareHeight = 100;
 
-    int directionOfBallForY = randomFunction(); // If 1 - up btw 2 - down
-    int directionOfBallForX = randomFunction(); // If 1 - up btw 2 - down
+    int directionOfBallForY = randomFunction(1, 2); // If 1 - up btw 2 - down
+    int directionOfBallForX = randomFunction(1, 2); // If 1 - up btw 2 - down
     
-    Vector2 ballPosition = { ballRadius, ballRadius };
+    Vector2 ballPosition = { screenWidth / 2, screenHeight / 2 };
+    Vector2 firstHalfSquarePosition = { halfSquareWidth + 5, screenHeight / 2 - halfSquareHeight + 30};
 
     InitWindow(screenWidth, screenHeight, "Ping Pong with C++ and raylib");
 
-    SetTargetFPS(60);
+    SetTargetFPS(screenFPS);
 
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // X coords
         if (directionOfBallForX == 2 && ballPosition.x <= screenWidth - ballRadius)
-            ballPosition.x += 2.0f;
+            ballPosition.x += ballSpeed;
         else
             directionOfBallForX = 1;
 
         if (directionOfBallForX == 1 && ballPosition.x >= ballRadius)
-            ballPosition.x -= 2.0f;
+            ballPosition.x -= ballSpeed;
         else
             directionOfBallForX = 2;
 
         // Y coords
         if (directionOfBallForY == 2 && ballPosition.y <= screenHeight - ballRadius)
-            ballPosition.y += 2.0f;
+            ballPosition.y += ballSpeed;
         else 
             directionOfBallForY = 1;
 
         if (directionOfBallForY == 1 && ballPosition.y >= ballRadius)
-            ballPosition.y -= 2.0f;
+            ballPosition.y -= ballSpeed;
         else 
             directionOfBallForY = 2;
 
-        // Drawing space
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        DrawText("WASD", 10, 10, 20, DARKGRAY);
+        DrawText("WASD and Arrows", 10, 10, 20, DARKGRAY);
 
+        // Draw gaming ball
         DrawCircleV(ballPosition, ballRadius, BLACK);
+
+        // Draw first HalfSquare
+        DrawRectangle(firstHalfSquarePosition.x, firstHalfSquarePosition.y, halfSquareWidth, halfSquareHeight, BLACK);
 
         EndDrawing();
     }
@@ -65,11 +74,11 @@ void main()
     CloseWindow();
 }
 
-int randomFunction()
+int randomFunction(int minValue, int maxValue)
 {
     std::random_device dev;
     std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist6(1, 2);
+    std::uniform_int_distribution<std::mt19937::result_type> dist6(minValue, maxValue);
 
     return dist6(rng);
 }
